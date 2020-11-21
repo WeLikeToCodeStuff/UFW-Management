a2c1187da8f7c2121b2ce315c962605d6be73bdf
Jamie L <51766327+DeveloperNeon@users.noreply.github.com>
2020-11-21 10:08:43 -0600

    Create ufw.sh

---
+++ ufw.sh
@@ -0,0 +1,49 @@
+#Copyright NeonDevelopment & SsmidgeXYZ 2020
+#PORT
+read -p "Would you like to deny or to accept a port? Type deny to deny and accept to accept." denyaccept
+
+echo;echo
+echo "You entered $denyaccept"
+read -p "Is this correct? y/n " denyacceptans
+if [ $denyacceptans == "y" ]
+then
+echo
+else
+echo
+fi
+
+
+
+#Port
+
+
+read -p "What is the port you would like to $denyacceptans"? port
+
+
+echo;echo
+echo "You entered $port"
+read -p "Is this correct? y/n " portans
+if [ $portans == "y" ]
+then
+echo
+if [ $portans == "n" ]
+then 
+else "Canceling..."
+echo
+fi
+
+
+
+while(true); do
+    if [$denyaccept == "deny"]
+    then
+        ufw deny $port
+    fi
+
+    if [$denyaccept == "allow"]
+    then
+    ufw allow $port
+    fi
+done
+
+#Copyright NeonDevelopment & SsmidgeXYZ 2020
