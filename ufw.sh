07510bd818abed044236fb07627763ca6a38c6cb
Adrian T <43932476+Ssmidge@users.noreply.github.com>
2020-11-21 11:31:18 -0500

    Made it work

--- ufw.sh
+++ ufw.sh
@@ -1,19 +1,19 @@
+#!/bin/bash
 #Copyright NeonDevelopment & SsmidgeXYZ 2020
 #PORT
-read -p "Would you like to deny or to accept a port? Type deny to deny and accept to accept." denyaccept
+read -p "Would you like to deny or to accept a port? Type deny to deny and accept to accept. " denyaccept
+echo
 
 echo;echo
 echo "You entered $denyaccept"
+echo
 read -p "Is this correct? y/n " denyacceptans
+echo
 if [ $denyacceptans == "y" ]
-then
-echo
-else
-if [ $denyacceptans == "n" ]
-then 
-echo "Canceling..."
-exit
-echo
+    then
+    echo
+    else
+    echo
 fi
 
 
@@ -21,34 +21,40 @@ fi
 #Port
 
 
-read -p "What is the port you would like to $denyaccept"? port
+read -p "What is the port you would like to $denyaccept " port
+echo
 
 
 echo;echo
-echo "You entered $port"
+echo "You entered $port "
 read -p "Is this correct? y/n " portans
+echo
+
 if [ $portans == "y" ]
-then
-echo
+    then
+    echo
+fi
+
 if [ $portans == "n" ]
-then 
-echo "Canceling..."
-exit
-echo
+    then 
+    echo "Canceling..."
+    exit 0
+    echo
+fi
+
+
+if [[ $denyaccept == *"deny"* ]]; then
+        echo Successfully denied $port
+        ufw deny $port
+        exit 0
+fi
+
+if [[ $denyaccept == *"allow"* ]]; then
+        ufw allow $port
+        echo Successfully allowed $port
+        exit 0
 fi
 
 
 
-while(true); do
-    if [$denyaccept == "deny"]
-    then
-        ufw deny $port
-    fi
-
-    if [$denyaccept == "allow"]
-    then
-    ufw allow $port
-    fi
-done
-
-#Copyright NeonDevelopment & SsmidgeXYZ 2020
+exit 0
